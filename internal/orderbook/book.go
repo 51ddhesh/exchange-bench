584458e21ffd5bf7f73c81fd66d515ce0073a133
siddhesh <173941233+51ddhesh@users.noreply.github.com>
2026-05-20 12:06:00 +0530

    [feat/orderbook/book]: update Cancel signature to uint64

--- internal/orderbook/book.go
+++ internal/orderbook/book.go
@@ -3,7 +3,7 @@ package orderbook
 // standard price-time priority reference orderbook
 type Book interface {
 	Add(o Order) (fills []Fill, rests bool)
-	Cancel(OrderID string) CancelResult
+	Cancel(orderID uint64) CancelResult
 	BestBid() int64
 	BestAsk() int64
 	Depth(n int) (bids, asks [][2]int64)
