9ae117beddfd43e04fc59cdeccbce683b34c52d3
siddhesh <173941233+51ddhesh@users.noreply.github.com>
2026-05-28 22:51:41 +0530

    [cmd/worker]: add worker gRPC server

---
+++ cmd/worker/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/51ddhesh/exchange-bench/internal/botworker"
+	"github.com/51ddhesh/exchange-bench/internal/coordinator/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	listen := flag.String("listen", ":9090", "host:port to serve gRPC on")
+	workerID := flag.String("worker-id", hostname(), "human-readable worker name")
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", *listen)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "worker: listen %s: %v\n", *listen, err)
+		os.Exit(1)
+	}
+
+	srv := grpc.NewServer()
+	proto.RegisterWorkerServiceServer(srv, botworker.NewWorkerServer(*workerID))
+
+	fmt.Printf("[worker %s] listening on %s\n", *workerID, *listen)
+
+	if err := srv.Serve(lis); err != nil {
+		fmt.Fprintf(os.Stderr, "worker: serve: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func hostname() string {
+	h, err := os.Hostname()
+
+	if err != nil {
+		return "unknown"
+	}
+
+	return h
+}
