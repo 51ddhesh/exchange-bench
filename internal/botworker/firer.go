2c7defb4e0cdefdb422be520b5684d7c9a82e50a
siddhesh <173941233+51ddhesh@users.noreply.github.com>
2026-05-28 19:45:22 +0530

    [feat/botworker/firer]: Implement open loop firer

--- internal/botworker/firer.go
+++ internal/botworker/firer.go
@@ -8,6 +8,9 @@ import (
 	"time"
 
 	"github.com/51ddhesh/exchange-bench/internal/coordinator/proto"
+	"github.com/51ddhesh/exchange-bench/internal/protocol"
+	"github.com/51ddhesh/exchange-bench/internal/runner"
+	"github.com/51ddhesh/exchange-bench/internal/workload"
 	"github.com/HdrHistogram/hdrhistogram-go"
 )
 
@@ -40,12 +43,16 @@ func (f *inFlight) pop(orderID string) (int64, bool) {
 }
 
 type firer struct {
-	ticks  []proto.Tick
-	image  string
-	rate   atomic.Int32
-	hist   *hdrhistogram.Histogram
-	events chan TelemetryEvent
-	done   chan struct{}
+	ticks      []proto.Tick
+	image      string
+	rate       atomic.Int32
+	ticksSent  atomic.Int64
+	ticksAcked atomic.Int64
+	timedOut   atomic.Bool
+	hist       *hdrhistogram.Histogram
+	histMu     sync.Mutex
+	events     chan TelemetryEvent
+	done       chan struct{}
 }
 
 func newFirer(ticks []*proto.Tick, image string, initialRate int32) *firer {
@@ -66,19 +73,126 @@ func newFirer(ticks []*proto.Tick, image string, initialRate int32) *firer {
 	return f
 }
 
-// Run waits until fireAtUnixNs then fires all ticks open-loop.
-// Closes f.events when done.
+// Run spin-waits until fireAtUnixNs, starts the sandbox, then launches the
+// writer and reader goroutines. Writer fires ticks open-loop; reader collects
+// ACKs and records RTT. Returns after all ticks are fired and all ACKs drained.
 func (f *firer) Run(ctx context.Context, fireAtUnixNs int64) error {
-	// spin-wait for synchronized start
 	for time.Now().UnixNano() < fireAtUnixNs {
 		runtime.Gosched()
 	}
 
-	// TODO: open-loop writer+reader goroutines
-	close(f.events)
-	close(f.done)
+	sb, err := runner.StartSandbox(ctx, f.image)
+	if err != nil {
+		close(f.events)
+		close(f.done)
+		return err
+	}
 
+	inf := &inFlight{}
+	reader := protocol.NewReader(sb.Stdout())
+
+	// Reader goroutine
+	var readerWg sync.WaitGroup
+	readerWg.Add(1)
+	go func() {
+		defer readerWg.Done()
+		defer close(f.events)
+
+		for {
+			resp, err := reader.ReadResponse()
+			if err != nil {
+				return
+			}
+
+			if resp.Type != protocol.RespACK && resp.Type != protocol.RespREJ {
+				continue
+			}
+
+			receivedAt := time.Now().UnixNano()
+			intendedAt, ok := inf.pop(resp.OrderID)
+
+			if !ok {
+				continue
+			}
+
+			deltaUs := (receivedAt - intendedAt) / 1000
+			if deltaUs < 1 {
+				deltaUs = 1
+			}
+
+			f.histMu.Lock()
+			f.hist.RecordValue(deltaUs)
+			f.histMu.Unlock()
+
+			acked := resp.Type == protocol.RespACK
+
+			if acked {
+				f.ticksAcked.Add(1)
+			}
+
+			f.events <- TelemetryEvent{
+				OrderID:      resp.OrderID,
+				IntendedAtNs: intendedAt,
+				ReceivedAtNs: receivedAt,
+				Acked:        acked,
+			}
+		}
+	}()
+
+	// Open loop writer
+	currentRate := f.rate.Load()
+	ticker := time.NewTicker(tickInterval(currentRate))
+	defer ticker.Stop()
+
+	for i := range f.ticks {
+		select {
+		case <-ctx.Done():
+			f.timedOut.Store(true)
+			goto writerDone
+
+		case <-ticker.C:
+		}
+
+		if newRate := f.rate.Load(); newRate != currentRate {
+			currentRate = newRate
+			ticker.Reset(tickInterval(currentRate))
+		}
+
+		tick := protoToWorkload(f.ticks[i])
+		intendedAt := time.Now().UnixNano()
+		inf.store(tick.OrderID, intendedAt)
+		f.ticksSent.Add(1)
+
+		if err := protocol.WriteTick(sb.Stdin(), tick); err != nil {
+			goto writerDone
+		}
+	}
+
+writerDone:
+	sb.Stdin().Close() // EOF
+	sb.Kill()
+	readerWg.Wait()
+	close(f.done)
 	return nil
+}
+
+func tickInterval(ratePerSec int32) time.Duration {
+	if ratePerSec <= 0 {
+		return time.Second
+	}
+
+	return time.Second / time.Duration(ratePerSec)
+}
+
+func protoToWorkload(t proto.Tick) workload.Tick {
+	return workload.Tick{
+		Type:    workload.TickType(t.Type),
+		OrderID: t.OrderId,
+		Side:    byte(t.Side),
+		OrdType: byte(t.OrdType),
+		Price:   t.Price,
+		Qty:     t.Qty,
+	}
 }
 
 func (f *firer) SetRate(ratePerSec int32) {
@@ -91,10 +205,15 @@ func (f *firer) Events() <-chan TelemetryEvent {
 
 func (f *firer) Metrics() firerMetrics {
 	<-f.done
+	f.histMu.Lock()
+	defer f.histMu.Unlock()
 
 	return firerMetrics{
+		TicksSent:    f.ticksSent.Load(),
+		TicksAcked:   f.ticksAcked.Load(),
 		P50LatencyUs: f.hist.ValueAtQuantile(50),
 		P90LatencyUs: f.hist.ValueAtQuantile(90),
 		P99LatencyUs: f.hist.ValueAtQuantile(99),
+		TimedOut:     f.timedOut.Load(),
 	}
 }
